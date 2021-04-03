0e5c9e24ec5f5c330814ee5d76494d823b146c9d
eslam <eslam.magdy@businessvalley.com>
2021-04-04 01:31:10 +0200

    add https to position stack api

--- src/positionStack.js
+++ src/positionStack.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
 const pos = Vue.axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/http://api.positionstack.com/v1',
+    baseURL: 'https://api.positionstack.com/v1',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
